cdfdba89293a76db6aab53e2fdd90c693b893584
rapsealk <piono623@naver.com>
2016-05-21 15:35:55 +0900

    Add Database
    Table Question

--- hangman/models.py
+++ hangman/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):   # table class inherits models.Model
-    name = models.CharField(max_length=30)  # default lenght = 30
+    name = models.CharField(max_length=30)  # default length = 30
     score = models.IntegerField(default=0)  # default value = 0
 
     def __unicode__(self):  # __str__ on Python 3
@@ -14,7 +14,9 @@ class User(models.Model):   # table class inherits models.Model
 
 class Question(models.Model):
     word = models.CharField(max_length=30)
+    length = models.IntegerField(default=0)
     hint = models.CharField(max_length=200)
+    key = models.IntegerField(default=0)
     
     def __unicode__(self):
         return "%s: %s" %(self.word, self.hint)
